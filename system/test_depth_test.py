0af1a06e279ab7bf7a6ac6482ed85fad1358c568
anderson-silva21 <andsoares2001@gmail.com>
2023-09-12 11:12:24 -0300

    analise novas imagens

---
+++ system/test_depth_test.py
@@ -0,0 +1,31 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# img3 = cv2.imread('../sequenciaTRES/maps/figuraUm_map.png')
+# img3 = cv2.imread('../mapMeioMetroEUmMetro.png')
+
+# Tente abrir e carregar a primeira imagem
+img3 = cv2.imread('../sequenciaTRES/maps/figuraUm_map.png')
+if img3 is not None:
+    plt.imshow(img3)
+else:
+    print("Não foi possível abrir a primeira imagem.")
+
+# Plote 5 linhas da imagem
+if img3 is not None:
+    plt.figure()
+    plt.plot(img3[580, :])
+    plt.plot(img3[680, :])
+
+
+'''
+# Tente abrir e carregar a segunda imagem
+img4 = cv2.imread('../sequenciaTRES/figuraUm.jpeg')
+if img4 is not None:
+    plt.figure()
+    plt.imshow(img4)
+else:
+    print("Não foi possível abrir a segunda imagem.")
+'''
+
+plt.show()
