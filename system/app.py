87a30908ee07b1456ea457b523a4326ddf13caef
anderson-silva21 <andsoares2001@gmail.com>
2023-09-26 11:27:41 -0300

    servidor arrumado

--- system/app.py
+++ system/app.py
@@ -1,59 +1,42 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from queue import PriorityQueue
-import torch
-import os
-import glob
 from flask import Flask, request, jsonify
-from werkzeug.utils import secure_filename
-
-midas = torch.hub.load('intel-isl/MiDaS', 'MiDaS_small')
-midas.to('cpu')
-midas.eval()
-
-transforms = torch.hub.load('intel-isl/MiDaS', 'transforms')
-transform = transforms.small_transform
+import os
+import base64
+import io
+from PIL import Image
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+def save_base64_image(base64data, filename):
+    try:
+        image_data = base64.b64decode(base64data)
+        image_stream = io.BytesIO(image_data)
+        image = Image.open(image_stream)
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename), 'PNG')
+        return True
+    except Exception as e:
+        print(f"Error saving image: {str(e)}")
+        return False
 
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
+    data = request.json
+    if 'image' in data:
+        image_data = data['image']
+        if save_base64_image(image_data, 'uploaded_image.png'):
+            return jsonify({"message": "Image uploaded and saved successfully."}), 200
+        else:
+            return jsonify({"error": "Failed to save the image."}), 500
+    else:
+        return jsonify({"error": "Invalid JSON data. Make sure it contains 'image' field."}), 400
 
-    file = request.files['file']
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
-
-    if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        frame = cv2.imread(image_path)
-
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        imgbatch = transform(img).to('cpu')
-
-        with torch.no_grad():
-            prediction = midas(imgbatch)
-            prediction = torch.nn.functional.interpolate(
-                prediction.unsqueeze(1),
-                size=img.shape[:2],
-                mode='bicubic',
-                align_corners=False
-            ).squeeze()
-
-            output = prediction.cpu().numpy()
-            output = (output - output.min()) / (output.max() - output.min())
-            output = (255 * output).astype(np.uint8)
-
-        output_filename = os.path.splitext(filename)[0] + "_map.png"
-        output_path = os.path.join(
-            app.config['UPLOAD_FOLDER'], output_filename)
-        cv2.imwrite(output_path, output)
-
-        return jsonify({'message': f'Saved {output_path}'})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
